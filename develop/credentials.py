2cf45f94d347527baf209df52a36f238fe149b56
mahoutukaisali <mahoutukaisali@gmail.com>
2020-03-16 00:33:17 +0900

    create device_manufacturer, device_types and device from csv

--- develop/credentials.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def credentials():
-  login = {'APIENDPOINT':'http://localhost:32770', 'NETBOXURL':'http://localhost:32770', 'NETBOXTOKEN':'0123456789abcdef0123456789abcdef01234567'}
-  return login
