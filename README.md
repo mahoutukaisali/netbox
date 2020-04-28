08099502dae2037f36faebe1adee919834b82dc3
mahoutukaisali <mahoutukaisali@gmail.com>
2020-04-29 00:44:43 +0900

    added readme

--- README.md
+++ README.md
@@ -3,13 +3,45 @@ I built this application as a kick start Netbox tool with our network devices.
 
 ![GitHub Logo](mynetbox_slide.png)
 
-## Functions
-In main2.py file 
+## Dependencies
+This project relies on following packages
+- Netbox (including pynetbox version `4.2.5`)
+- pyATS - version must be at least `20.1`
+- Docker - version must be at least `17.05`<br>
 
-The technologies used in this demonstration are:
+## Project Goals
+I have been wanted to manage a bulk of network device's inventories and port connection information by Netbox instead of is too complicated Excel files. So this project aims to register each devices of interface, site location, rack, and neighbors by CDP configuration.<br>
+ However the problem was how to register these amount of device's information into Netbox. By manual? That's not practical solution.  
+ Fortunatly I have excel files and pyATS awesome powerful tool can convert this excel file to Testbed file so I've decided to use them. Want more learning pyATS & Excel file? You can see: https://pubhub.devnetcloud.com/media/pyats-getting-started/docs/quickstart/manageconnections.html#creation-from-excel-file
+ 
+Specifically this application tackles the following.
 
-<liNetbox - A modern IPAM and DCIM tool, written in Python and providing robust programmability support through APIs and SDKs
-pyATS - A network verification and configuration tool used to gather operational and configuration details from a live network, as well as make configuration changes.
-Docker - A container technologies used to "package" up the application into a portable image that can be run nearly anywhere
-Cisco NX-OS - A programmable network operating system providing many features, including running containers on the switch.
-Webex Teams - An collaboration platform offering rich communication and programmability features allowing for robust ChatOps use cases.
+    Register device's manufacturer 
+    Register device's name, device type(such as Nexus-9000, catalyst-2950 etc.) and interface
+    Register device's CDP neighbor (which means register which interfaces and which interfaces are connected by cable)
+    Register site location and rack
+    
+## Running The Demo Yourself
+1. Prepare pyATS testbed
+- If you Run Using DevNet SandBox
+As you can see pyats/The-Open-NX-OS-Sandbox.yaml file, you can use this testbed to connect DevNet SandBox name "Open NX-OS Sandbox"
+
+- Otherwise please prepare testbed.yaml yourself  
+
+2. Prepare Netbox environment
+If you'd prepared Netbox by Docker, execute following commands to post api to Netbox.
+```
+export NETBOXURL='http://localhost:80'
+export NETBOXTOKEN='0123456789abcdef0123456789abcdef01234567'
+```
+
+3. Run Script
+Now you run the script by following command.
+```
+python main.py pyats/ 
+```
+**Note**: You'll might see that all of interfaces are **NOT** registered but several interfaces are registered because default main.py uses `collect_interface_only_up_status()` function which only registers up status interfaces.
+
+## Caveats / Known Issues / Later Updates / Note
+- Since netbox cannot register more than one cable for a single port, I excluded the cable registration for the management port. So you cannot see mgmt0 with cable status. I'll solve how to register them later.
+- Plan to add a function to register the rack information of the device.
--- main.py
+++ main.py
@@ -4,114 +4,60 @@
 # Released under the MIT license
 # https://opensource.org/licenses/mit-license.php
 
+#
+# Connection Setup Section
+#
+
 import argparse
+import os
+import pynetbox
+from ats.topology import loader
+from genie.testbed import load
+import netboxPackage.get_pyats as pyats
+import netboxPackage.netbox_utils as netbox
+import json
 
-parser = argparse.ArgumentParser(description='Process post parameters in netbox.')
-parser.add_argument('arg1', 
-                    help='Please specify the path of parameter csv file.')
-parser.add_argument('--arg2', 
-                    choices=['manufacturer', 'device_type', 'device', 'interface'],
-                    help='''which one would you want to post config ? 
-                            choose from: manufacturer, device type, device, interface. default => all
-                         '''
-                  )
+# Create netbox API object
+NETBOXURL = os.environ["NETBOXURL"]
+NETBOXTOKEN = os.environ["NETBOXTOKEN"]
+login_info = pynetbox.api(url=NETBOXURL, token=NETBOXTOKEN)
+netbox = netbox.Netbox(login_info)
 
-args = parser.parse_args()
+parser = argparse.ArgumentParser(description = "standalone parser")
+parser.add_argument('testbed', type = loader.load)
+args, unknown = parser.parse_known_args()
 
-if __name__ == "__main__":
-    import csv
-    import os
-    import sys
-    from my_netbox_module import netbox_utils
+'''
+Connection setup to DevNet sandbox
+'''
 
-    ## specify path of parameter_sheet which is csv file
-    csv_parameter_sheet = args.arg1
-    
-    with open(csv_parameter_sheet, 'r') as f:
-        dictData = csv.DictReader(f)
+testbed_file = args.testbed
+testbed = load(testbed_file)
+device_list = testbed.devices.keys()
 
-        if args.arg2 == None:
-          print('Start posting all of configurations.')
-          for data in dictData:
-            manufacturer = data.get('manufacturer')
-            device_role = data.get('role')
-            model = data.get('model')
-            site = data.get('site')
-            tenant = data.get('Tenant')
-            hostname = data.get('hostname')
-            interface = data.get('interface')
-            description = data.get("interface_desctiption")
-            interface_type = data.get('interface_type')
+pyats = pyats.PyATS(testbed)
 
-            if netbox_utils.create_netbox_manufacturer(manufacturer) == None:
-              print('Success: create_netbox_manufacturer')
-            else:
-              print('Failed: create_netbox_manufacturer')
-          
-            if netbox_utils.create_netbox_device_types(manufacturer, device_role, model) == None:
-              print('Success: create_netbox_device_types')
-            else:
-              print('Failed: create_netbox_device_types')
+for device in device_list:
+  testbed.devices[device].connect()
+  platform_data = pyats.platform_info(device)
+  model = pyats.parse_device_types(device)
+  netbox.create_netbox_device_types('Cisco', 'core', model)
+  netbox.create_netbox_device(device, 'core', 'US', 'San Francisco', model)
 
-            if netbox_utils.create_netbox_device(hostname, device_role, tenant, site, device_type=model) == None:
-              print('Success: create_netbox_device')
-            else:
-              print('Failed: create_netbox_device')
-            
-            if netbox_utils.create_netbox_interface(hostname, interface, interface_type) == None:
-              print('Success: create_netbox_interface')
-            else:
-              print('Failed: create_netbox_interface')
-          sys.exit(1)
+  for inter in pyats.collect_interface_only_up_status(device):
+    netbox.create_netbox_interface(device, inter, "100base-tx")
 
-        elif args.arg2 == 'manufacturer':
-          for data in dictData:
-            manufacturer = data.get('manufacturer')
-            #netbox_utils.create_netbox_manufacturer(manufacturer)
-            if netbox_utils.create_netbox_manufacturer(manufacturer) == None:
-              print('Success: create_netbox_manufacturer')
-            else:
-              print('Failed: create_netbox_manufacturer')
-          sys.exit(1)
+print("######Complete registering interface information! ######")
 
-        elif args.arg2 == 'device_type':
-          for data in dictData:
-            manufacturer = data.get('manufacturer')
-            device_role = data.get('role')
-            model = data.get('model')
-            #netbox_utils.create_netbox_device_types(manufacturer, device_role, model)
-            if netbox_utils.create_netbox_device_types(manufacturer, device_role, model) == None:
-              print('Success. The function name: create_netbox_device_types')
-            else:
-              print('Failed. The fuction name: create_netbox_device_types')
-          sys.exit(1)
+for device in device_list:
+  testbed.devices[device].connect()
+  for value in pyats.collect_cdp_neighbors(device)['index'].values():
+    ## If cdp config has mgmt0 and it has multiple connection with others, Netbox
+    # cannot manage it so excludes mgmt0 for now.
+    if value.get('local_interface') != "mgmt0":
+      local_interface_name = value.get('local_interface')
+      remote_device_name = value.get("system_name")
+      remote_interface_name = value.get('port_id')
+      netbox.create_netbox_cable(device, remote_device_name, local_interface_name, remote_interface_name)
 
-        elif args.arg2 == 'device':
-          for data in dictData:
-            device_role = data.get('role')
-            site = data.get('site')
-            tenant = data.get('Tenant')
-            hostname = data.get('hostname')
-            model = data.get('model')
-            #create_netbox_device(hostname, device_role, tenant, site, device_type=model)
-            if netbox_utils.create_netbox_device(hostname, device_role, tenant, site, device_type=model) == None:
-              print('Success. The function name: create_netbox_device')
-            else:
-              print('Failed. The fuction name: create_netbox_device')
-          sys.exit(1)
-               
-        elif args.arg2 == 'interface':
-          for data in dictData:
-            hostname = data.get('hostname')
-            interface = data.get('interface')
-            description = data.get("interface_desctiption")
-            interface_type = data.get('interface_type')
-            #create_netbox_interface(hostname, interface, interface_type)
-            if netbox_utils.create_netbox_interface(hostname, interface, interface_type) == None:
-              print('Success. The fuction name: create_netbox_interface')
-              #print(netbox_utils.create_netbox_interface(hostname, interface, interface_type))
-            else:
-              print('Failed. The fuction name: create_netbox_interface')
-              #print(netbox_utils.create_netbox_interface(hostname, interface, interface_type))
-
-          sys.exit(1)
+print("######Complete registering CDP information! ######")
