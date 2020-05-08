332fd5c8c67397d128c43bed4d70d030300b30c2
mahoutukaisali <mahoutukaisali@gmail.com>
2020-05-08 11:40:16 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 from os import path
 import re
 
 #package_name = "my_netbox_module"
 
-#root_dir = path.abspath(path.dirname(__file__))
-#
-#def _requirements():
-#    return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
+root_dir = path.abspath(path.dirname(__file__))
+filename = path.join(root_dir, 'requirement.txt')
+
+def _requirements(filename):
+    return open(filename).read().splitlines()
 
 setup(
     name="netboxPackage",
     version="1.0",
-    packages=find_packages("lib"),
+    packages=["netboxPackage"],
     package_dir={"": "lib"},
     include_package_data=True,
-    #install_requires=_requirements(),
+    install_requires=_requirements(filename),
 )
